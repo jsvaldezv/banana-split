8a185844362dfa0f4cdcace6c2fc997cc4590457
Jesús Valdez <jsvaldezv@yahoo.com>
2022-05-17 22:45:50 -0500

    Clean

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,15 +1,6 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-//==============================================================================
 MidiSynthAudioProcessor::MidiSynthAudioProcessor()
 #ifndef JucePlugin_PreferredChannelConfigurations
      : AudioProcessor (BusesProperties()
@@ -24,11 +15,8 @@ MidiSynthAudioProcessor::MidiSynthAudioProcessor()
 {
 }
 
-MidiSynthAudioProcessor::~MidiSynthAudioProcessor()
-{
-}
+MidiSynthAudioProcessor::~MidiSynthAudioProcessor(){}
 
-//==============================================================================
 const juce::String MidiSynthAudioProcessor::getName() const
 {
     return JucePlugin_Name;
@@ -68,8 +56,7 @@ double MidiSynthAudioProcessor::getTailLengthSeconds() const
 
 int MidiSynthAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
 }
 
 int MidiSynthAudioProcessor::getCurrentProgram()
@@ -77,31 +64,18 @@ int MidiSynthAudioProcessor::getCurrentProgram()
     return 0;
 }
 
-void MidiSynthAudioProcessor::setCurrentProgram (int index)
-{
-}
+void MidiSynthAudioProcessor::setCurrentProgram ([[maybe_unused]] int index){}
 
-const juce::String MidiSynthAudioProcessor::getProgramName (int index)
+const juce::String MidiSynthAudioProcessor::getProgramName ([[maybe_unused]] int index)
 {
     return {};
 }
 
-void MidiSynthAudioProcessor::changeProgramName (int index, const juce::String& newName)
-{
-}
+void MidiSynthAudioProcessor::changeProgramName ([[maybe_unused]] int index, [[maybe_unused]] const juce::String& newName){}
 
-//==============================================================================
-void MidiSynthAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
-{
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
-}
+void MidiSynthAudioProcessor::prepareToPlay ([[maybe_unused]] double sampleRate, [[maybe_unused]] int samplesPerBlock){}
 
-void MidiSynthAudioProcessor::releaseResources()
-{
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
-}
+void MidiSynthAudioProcessor::releaseResources(){}
 
 #ifndef JucePlugin_PreferredChannelConfigurations
 bool MidiSynthAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
@@ -110,58 +84,35 @@ bool MidiSynthAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts
     juce::ignoreUnused (layouts);
     return true;
   #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
-
-    // This checks if the input layout matches the output layout
    #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
    #endif
-
     return true;
   #endif
 }
 #endif
 
-void MidiSynthAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+void MidiSynthAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer,
+                                            [[maybe_unused]] juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
+    
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    
+    
 }
 
-//==============================================================================
 bool MidiSynthAudioProcessor::hasEditor() const
 {
-    return true; // (change this to false if you choose to not supply an editor)
+    return true;
 }
 
 juce::AudioProcessorEditor* MidiSynthAudioProcessor::createEditor()
@@ -169,22 +120,10 @@ juce::AudioProcessorEditor* MidiSynthAudioProcessor::createEditor()
     return new MidiSynthAudioProcessorEditor (*this);
 }
 
-//==============================================================================
-void MidiSynthAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
-}
+void MidiSynthAudioProcessor::getStateInformation ([[maybe_unused]] juce::MemoryBlock& destData){}
 
-void MidiSynthAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
-{
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
-}
+void MidiSynthAudioProcessor::setStateInformation ([[maybe_unused]] const void* data, [[maybe_unused]] int sizeInBytes){}
 
-//==============================================================================
-// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new MidiSynthAudioProcessor();
