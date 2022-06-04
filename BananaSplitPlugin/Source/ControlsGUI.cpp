7cb69aff035040c94289e94c184d5805f3502f4f
Jesús Valdez <jsvaldezv@yahoo.com>
2022-06-03 19:29:39 -0500

    Colores sliders and buttons

--- BananaSplitPlugin/Source/ControlsGUI.cpp
+++ BananaSplitPlugin/Source/ControlsGUI.cpp
@@ -15,36 +15,57 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
 {
     gain.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     gain.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    gain.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    gain.setColour(juce::Slider::backgroundColourId, juce::Colours::lightyellow);
+    gain.setColour(juce::Slider::trackColourId, juce::Colours::yellowgreen);
     addAndMakeVisible(gain);
     gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Gain", gain);
     
     attack.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     attack.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    attack.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    attack.setColour(juce::Slider::backgroundColourId, juce::Colours::blue);
+    attack.setColour(juce::Slider::trackColourId, juce::Colours::red);
     addAndMakeVisible(attack);
     attackAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Attack", attack);
     
     decay.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     decay.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    decay.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    decay.setColour(juce::Slider::backgroundColourId, juce::Colours::blue);
+    decay.setColour(juce::Slider::trackColourId, juce::Colours::red);
     addAndMakeVisible(decay);
     decayAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Decay", decay);
     
     sustain.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     sustain.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    sustain.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    sustain.setColour(juce::Slider::backgroundColourId, juce::Colours::blue);
+    sustain.setColour(juce::Slider::trackColourId, juce::Colours::red);
     addAndMakeVisible(sustain);
     sustainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Sustain", sustain);
     
     release.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     release.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    release.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    release.setColour(juce::Slider::backgroundColourId, juce::Colours::blue);
+    release.setColour(juce::Slider::trackColourId, juce::Colours::red);
     addAndMakeVisible(release);
     releaseAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Release", release);
     
     fmFreq.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     fmFreq.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    fmFreq.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    fmFreq.setColour(juce::Slider::backgroundColourId, juce::Colours::blue);
+    fmFreq.setColour(juce::Slider::trackColourId, juce::Colours::red);
     addAndMakeVisible(fmFreq);
     fmFreqAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "FmFreq", fmFreq);
     
     fmDepth.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     fmDepth.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    fmDepth.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
+    fmDepth.setColour(juce::Slider::backgroundColourId, juce::Colours::blue);
+    fmDepth.setColour(juce::Slider::trackColourId, juce::Colours::red);
     addAndMakeVisible(fmDepth);
     fmDepthAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "FmDepth", fmDepth);
 }
--- BananaSplitPlugin/Source/FxGUI.cpp
+++ BananaSplitPlugin/Source/FxGUI.cpp
@@ -15,22 +15,30 @@ FxGUI::FxGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
 {
     reverb.setButtonText("Reverb");
     reverb.setClickingTogglesState(true);
+    reverb.setColour(juce::TextButton::buttonColourId, juce::Colours::black);
+    reverb.setColour(juce::TextButton::buttonOnColourId, juce::Colours::green);
     addAndMakeVisible(reverb);
     reverbAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.apvt, "Reverb", reverb);
     
     chorus.setButtonText("Chorus");
     chorus.setClickingTogglesState(true);
+    chorus.setColour(juce::TextButton::buttonColourId, juce::Colours::black);
+    chorus.setColour(juce::TextButton::buttonOnColourId, juce::Colours::green);
     addAndMakeVisible(chorus);
     chorusAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.apvt, "Chorus", chorus);
     
     
     delay.setButtonText("Delay");
     delay.setClickingTogglesState(true);
+    delay.setColour(juce::TextButton::buttonColourId, juce::Colours::black);
+    delay.setColour(juce::TextButton::buttonOnColourId, juce::Colours::green);
     addAndMakeVisible(delay);
     delayAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.apvt, "Delay", delay);
     
     distortion.setButtonText("Distortion");
     distortion.setClickingTogglesState(true);
+    distortion.setColour(juce::TextButton::buttonColourId, juce::Colours::black);
+    distortion.setColour(juce::TextButton::buttonOnColourId, juce::Colours::green);
     addAndMakeVisible(distortion);
     distorAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(audioProcessor.apvt, "Distortion", distortion);
 }
