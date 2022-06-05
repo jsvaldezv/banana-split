86f4dc59a27702e11de29f5d74778f1180389b7a
Jesús Valdez <jsvaldezv@yahoo.com>
2022-06-05 00:16:22 -0500

    sizes

--- BananaSplitPlugin/Source/ControlsGUI.cpp
+++ BananaSplitPlugin/Source/ControlsGUI.cpp
@@ -21,6 +21,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     addAndMakeVisible(gain);
     gainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Gain", gain);
     
+    gainLabel.setText("Gain", juce::dontSendNotification);
+    gainLabel.setJustificationType(juce::Justification::centred);
+    gainLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    gainLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(gainLabel);
+    
     attack.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     attack.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     attack.setColour(juce::Slider::thumbColourId, juce::Colours::hotpink);
@@ -28,6 +34,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     attack.setColour(juce::Slider::trackColourId, juce::Colours::deeppink);
     addAndMakeVisible(attack);
     attackAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Attack", attack);
+    
+    attackLabel.setText("Attack", juce::dontSendNotification);
+    attackLabel.setJustificationType(juce::Justification::centred);
+    attackLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    attackLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(attackLabel);
     
     decay.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     decay.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -37,6 +49,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     addAndMakeVisible(decay);
     decayAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Decay", decay);
     
+    decayLabel.setText("Decay", juce::dontSendNotification);
+    decayLabel.setJustificationType(juce::Justification::centred);
+    decayLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    decayLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(decayLabel);
+    
     sustain.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     sustain.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     sustain.setColour(juce::Slider::thumbColourId, juce::Colours::orange);
@@ -44,6 +62,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     sustain.setColour(juce::Slider::trackColourId, juce::Colours::darkorange);
     addAndMakeVisible(sustain);
     sustainAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Sustain", sustain);
+    
+    sustainLabel.setText("Sustain", juce::dontSendNotification);
+    sustainLabel.setJustificationType(juce::Justification::centred);
+    sustainLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    sustainLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(sustainLabel);
     
     release.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     release.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
@@ -53,6 +77,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     addAndMakeVisible(release);
     releaseAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "Release", release);
     
+    releaseLabel.setText("Release", juce::dontSendNotification);
+    releaseLabel.setJustificationType(juce::Justification::centred);
+    releaseLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    releaseLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(releaseLabel);
+    
     fmFreq.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     fmFreq.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     fmFreq.setColour(juce::Slider::thumbColourId, juce::Colours::purple);
@@ -61,6 +91,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     addAndMakeVisible(fmFreq);
     fmFreqAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "FmFreq", fmFreq);
     
+    fmFreqLabel.setText("FM Freq", juce::dontSendNotification);
+    fmFreqLabel.setJustificationType(juce::Justification::centred);
+    fmFreqLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    fmFreqLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(fmFreqLabel);
+    
     fmDepth.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
     fmDepth.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     fmDepth.setColour(juce::Slider::thumbColourId, juce::Colours::yellow);
@@ -68,6 +104,12 @@ ControlsGUI::ControlsGUI(MidiSynthAudioProcessor& p) : audioProcessor(p)
     fmDepth.setColour(juce::Slider::trackColourId, juce::Colours::yellow);
     addAndMakeVisible(fmDepth);
     fmDepthAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(audioProcessor.apvt, "FmDepth", fmDepth);
+    
+    fmDepthLabel.setText("FM Depth", juce::dontSendNotification);
+    fmDepthLabel.setJustificationType(juce::Justification::centred);
+    fmDepthLabel.setColour(juce::Label::textColourId, juce::Colours::white);
+    fmDepthLabel.setFont(juce::Font (10.0f, juce::Font::plain));
+    addAndMakeVisible(fmDepthLabel);
 }
 
 ControlsGUI::~ControlsGUI(){}
@@ -79,13 +121,24 @@ void ControlsGUI::paint (juce::Graphics& g)
 
 void ControlsGUI::resized()
 {
-    gain.   setBoundsRelative(0.05f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
+    gain.           setBoundsRelative(0.06f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    gainLabel.      setBoundsRelative(0.058f, 0.865f, 0.1f, 0.1f);
     
-    attack. setBoundsRelative(0.185f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
-    decay.  setBoundsRelative(0.32f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
-    sustain.setBoundsRelative(0.455f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
-    release.setBoundsRelative(0.59f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
+    attack.         setBoundsRelative(0.185f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    attackLabel.    setBoundsRelative(0.185f, 0.865f, 0.1f, 0.1f);
     
-    fmFreq. setBoundsRelative(0.725f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
-    fmDepth.setBoundsRelative(0.86f, 0.5f - (0.9f/2.0f), 0.1f, 0.9f);
+    decay.          setBoundsRelative(0.33f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    decayLabel.     setBoundsRelative(0.33f, 0.865f, 0.1f, 0.1f);
+    
+    sustain.        setBoundsRelative(0.475f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    sustainLabel.   setBoundsRelative(0.475f, 0.865f, 0.1f, 0.1f);
+    
+    release.        setBoundsRelative(0.597f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    releaseLabel.   setBoundsRelative(0.595f, 0.865f, 0.1f, 0.1f);
+    
+    fmFreq.         setBoundsRelative(0.725f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    fmFreqLabel.    setBoundsRelative(0.725f, 0.865f, 0.1f, 0.1f);
+    
+    fmDepth.        setBoundsRelative(0.86f, 0.5f - (0.9f/2.0f), 0.1f, 0.8f);
+    fmDepthLabel.   setBoundsRelative(0.86f, 0.865f, 0.1f, 0.1f);
 }
--- BananaSplitPlugin/Source/ControlsGUI.h
+++ BananaSplitPlugin/Source/ControlsGUI.h
@@ -27,24 +27,31 @@ private:
     MidiSynthAudioProcessor& audioProcessor;
     
     juce::Slider gain;
+    juce::Label gainLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> gainAttachment;
     
     juce::Slider attack;
+    juce::Label attackLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attackAttachment;
     
     juce::Slider decay;
+    juce::Label decayLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> decayAttachment;
     
     juce::Slider sustain;
+    juce::Label sustainLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> sustainAttachment;
     
     juce::Slider release;
+    juce::Label releaseLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> releaseAttachment;
     
     juce::Slider fmFreq;
+    juce::Label fmFreqLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> fmFreqAttachment;
     
     juce::Slider fmDepth;
+    juce::Label fmDepthLabel;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> fmDepthAttachment;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ControlsGUI)
